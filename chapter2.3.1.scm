d2fbc4184884b79e4dc961c94b079c40875c48fa
Becca Bainbridge <beccabainbridge@gmail.com>
2013-12-04 13:56:21 -0500

    init exercises from chapter 2.3.1

---
+++ chapter2.3.1.scm
@@ -0,0 +1,38 @@
+#lang scheme
+
+(define (memq item x)
+  (cond ((null? x) #f)
+        ((eq? item (car x)) x)
+        (else (memq item (cdr x)))))
+
+; exercise 2.53
+
+(list 'a 'b 'c)
+; (a b c)
+
+(list (list 'george))
+; ((george))
+
+(cdr '((x1 x2) (y1 y2)))
+; ((y1 y2))
+
+(cadr '((x1 x2) (y1 y2)))
+; (y1 y2)
+
+(pair? (car '(a short list)))
+; #f
+
+(memq 'red '((red shoes) (blue socks)))
+; #f
+
+(memq 'red '(red shoes blue socks))
+; (red shoes blue socks)
+
+; exercise 2.54
+
+(define (equal? a b)
+  (cond ((and (null? a) (null? b)) #t)
+        ((or (null? a) (null? b)) #f)
+        ((and (pair? a) (pair? b)) (and (equal? (car a) (car b))
+                                        (equal? (cdr a) (cdr b))))
+        (else (eq? a b))))
