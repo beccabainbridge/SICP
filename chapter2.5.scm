0176a7571e9936fa4239ac8758d7958baa35f2af
Becca Bainbridge <beccabainbridge@gmail.com>
2013-12-17 15:27:49 -0500

    added exercises 2.81-2.88

--- chapter2.5.scm
+++ chapter2.5.scm
@@ -39,7 +39,7 @@
     (and (= (real-part x) (real-part y)) (= (imag-part y) (imag-part x))))
   (put 'equ? '(complex complex) equ?1)
   'done)
-  
+
 (define (equ? x y) (apply-generic 'equ? x y))
 
 ; exercise 2.80
@@ -63,7 +63,148 @@
     (and (= (real-part x) 0) (= (imag-part x) 0)))
   (put '=zero? 'complex =zero?)
   'done)
-  
+
 (define (=zero? x y) (apply-generic '=zero? x y))
 
+; exercise 2.81
 
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tags args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+          (apply proc (map contents args))
+          (if (= (length args) 2)
+              (let ((type1 (car type-tags))
+                    (type2 (cadr type-tags))
+                    (a1 (car args))
+                    (a2 (cadr args)))
+                (if (eq? type1 type2)
+                    (error "No method for this type"
+                           (list op type-tags))
+                    (let ((t1->t2 (get-coercion type1 type2))
+                          (t2->t1 (get-coercion type2 type1)))
+                      (cond (t1->t2
+                             (apply-generic op (t1->t2 a1) a2))
+                            (t2->t1
+                         (apply-generic op a1 (t2->t1 a2)))
+                            (else
+                             (error "No method for these types"
+                                    (list op type-tags)))))))
+              (error "No method for these types"
+                     (list op type-tags)))))))
+
+; exercise 2.82
+
+(define (apply-generic-mult op . args)
+  (let ((type-tags (map type-tags args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+          (apply proc (map contents args))
+          (let ((coercion-type (get-coercion-type type-tags)))
+            (if coercion-type
+                (apply apply-generic
+                       (append (list op) (coerce-all coercion-type args)))
+                (error "No method for these types"
+                     (list op type-tags))))))))
+
+; exercise 2.83
+
+
+(define (install-scheme-number-package)
+  ;...
+  (put 'raise 'scheme-number
+       (lambda (x) ((get 'make 'rational) x 1)))
+  'done)
+
+(define (install-rational-package)
+  ;...
+  (put 'raise 'rational
+       (lambda (x) ((get 'make 'real) (/ (numer x) (denom x)))))
+  'done)
+
+(define (install-real-package)
+  ;...
+  (put 'raise 'real
+       (lambda (x) ((get 'make-from-real-imag 'complex) x 0)))
+  'done)
+
+(define (install-complex-package)
+  ;..
+  'done)
+
+(define (raise x) (apply-generic 'raise x))
+
+; exercise 2.84
+
+(define (apply-generic-mult op . args)
+  (let ((type-tags (map type-tags args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+          (apply proc (map contents args))
+          (let ((coercion-type (get-coercion-type type-tags)))
+            (if coercion-type
+                (apply apply-generic
+                       (append (list op) (coerce-all coercion-type args)))
+                (error "No method for these types"
+                       (list op type-tags))))))))
+
+; exercise 2.85
+
+(define (install-scheme-number-package)
+  ;...
+  'done)
+
+(define (install-rational-package)
+  ;...
+  (put 'project 'rational
+       (lambda (x) ((get 'make 'scheme-number) (floor (/ (numer x) (denom x))))))
+  'done)
+
+(define (install-real-package)
+  ;...
+  (put 'project 'real
+       (lambda (x) ((get 'make 'rational) x 1)))
+  'done)
+
+(define (install-complex-package)
+  ;...
+  (put 'project 'complex
+       (lambda (x) ((get 'make 'real) (real-part x))))
+  'done)
+
+(define (project x) (apply-generic 'project x))
+
+(define (drop x)
+  (let ((drop-raise (raise (drop x))))
+    (if (eq? drop-raise x)
+        (drop x)
+        x)))
+
+; exercise 2.86
+
+
+; exercise 2.87
+
+(define (install-polynomial-package)
+  ;...
+  (define (=zero? x)
+    (empty-termlist? (termlist x)))
+  (put '=zero? 'polynomial =zero?)
+  'done)
+
+; exercise 2.88
+
+(define (sub-poly p1 p2)
+  (if (same-variable (variable p1) (variable p2))
+      (make-poly (variable p1)
+                 (add-terms (term-list p1)
+                            (negate-terms (term-list p2))))
+      (error "Polys not in same var -- SUB-POLY"
+             (list p1 p2))))
+
+(define (negate-terms term-list)
+  (if (empty-termlist? term-list)
+      (the-empty-termlist)
+      (let ((next-term (first-term term-list)))
+        (adjoin-term (make-term (order next-term) (neg (coeff next-term)))
+                     (negate-terms (rest-terms term-list))))))
